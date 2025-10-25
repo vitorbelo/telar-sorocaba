a7f41607295e97cb446f1f1cdc8ee09e7c74e6b0
Vitor Belo <vitorbelo99@gmail.com>
2025-10-25 14:41:08 -0300

    chore: Update image configuration in next.config.ts to include formats and sizes; ensure trailing slash is enabled for consistent URL structure.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Esta linha é essencial!
   images: {
-    unoptimized: true, // Necessário para export
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  trailingSlash: true,
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
+  trailingSlash: true,
 };
 
 export default nextConfig;
